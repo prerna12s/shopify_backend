43099a48d65c97145b6e506c55e06cacedd21e01
prerna12s <tripathiprerna1203@gmail.com>
2025-07-29 21:32:59 +0530

    added models and connect mongodb

---
+++ server.js
@@ -0,0 +1,22 @@
+const express= require('express');
+
+const app = express();
+const cookieParser=require('cookie-parser');
+
+const db=require('./config/mongoose-connectionn');
+//for frontend files
+const path = require('path');
+//
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));    
+app.use(cookieParser());
+//for frontend files
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+//
+//USE ROUTES 
+
+app.use('/users',userSRouters);
+app.use('/owners',ownersRouters);
+app.use('/products',productsRouters);
